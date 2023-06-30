52bcfcc0901ef24b41f5cbc0af4e275dd3113731
Vinicius Evangelista <viniciusevangelista088@gmail.comgit config --global user.name Vinni>
2023-06-30 15:47:06 -0300

    Configs, conexão e esquemas de validação

--- src/app.js
+++ src/app.js
@@ -1,79 +1,53 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import Joi from "joi"
 
 
-// Criação do app
-const app = express()
+// Criação do app:
+const app = express();
+const log = console.log();
 
-// Configurações
-app.use(cors()) //Front-End free
-app.use(express.json()) //Server On
-dotenv.config() // Dotenv habilitado
+// Configurações:
+app.use(cors()); //Front-End free
+app.use(express.json()); //Server On
+dotenv.config(); // Dotenv habilitado
 
-// Conexão com o Banco
-const mongoClient = new MongoClient(process.env.DATABASE_URL);
-let db;
+// Conexão com o Banco:
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+try {
+    await mongoClient.connect();
+    log("MongoDB conectado e rodando");
+} catch(erro){
+    (erro) => log(erro);
+}
+const db = mongoClient.db()
 
-let promisse1 = mongoClient.connect();
-promisse1.then(() => db = mongoClient.db())
-promisse1.catch((erro) => console.log(erro.message))
-
-// Coisas Globais
-const participantes = [
-    {
-        name: 'João', 
-        lastStatus: 12313123
-    }
-]
-
-const mensagens = [
-    {
-        from: 'João', 
-        to: 'Todos', 
-        text: 'oi galera', 
-        type: 'message', 
-        time: '20:04:37'
-    }
-]
+// Formato dos Dados:
+const schemaParticipante = Joi.object({
+    name: Joi.string().required()
+})
+//let participante = {
+//    name: 'João', 
+//    lastStatus: 12313123
+//};
+const schemaMsg = Joi.object({
+    to: Joi.string().required(),
+    text: Joi.string().required(),
+    type: Joi.string().required().valid('message', 'private_message')
+})
+//let mensagem = {
+//    from: 'João', 
+//    to: 'Todos', 
+//    text: 'oi galera', 
+//    type: 'message', 
+//    time: '20:04:37'
+//};
 
 // Funções (endpoints)
-app.get("/", (req, res) => {
-	const promise = db.collection("").find().toArray()
-
-	promise.then(data => res.send(data))
-	promise.catch(err => res.status(500).send(err.message))
-})
-
-app.get("/receitas/:id", (req, res) => {
-	const { id } = req.params
-	const { auth } = req.headers
-
-	if (!auth) {
-		return res.status(401).send("Faça login!")
-	}
-
-	const receita = receitas.find((rec) => rec.id === Number(id))
-	res.send(receita)
-})
-
-app.post("/receitas", (req, res) => {
-	const { titulo, ingredientes, preparo } = req.body
-
-	if (!titulo || !ingredientes || !preparo) {
-		return res.status(422).send({ message: "Todos os campos são obrigatórios!!!" })
-	}
-
-	const novaReceita = { titulo, ingredientes, preparo }
-
-	const promise = db.collection("receitas").insertOne(novaReceita)
-	
-	promise.then(() => res.sendStatus(201))
-	promise.catch(err => res.status(500).send(err.message))
-})
 
 
-// Ligar a aplicação do servidor para ouvir requisições
+// Ligar a aplicação do servidor para ouvir requisições:
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Servidor está rodando na porta ${PORT}`));
